f25ec56d73db0f69192508e9680ad92d785ffe0f
Tanay <tanayghoriwala001@gmail.com>
2025-03-18 21:56:10 +0530

    fix:hopefully, this solves the routing issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,38 @@
+// import path from "path"
+// import tailwindcss from "@tailwindcss/vite"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react";
 
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/", 
+  base: "/",
   build: {
     outDir: "dist",
+    assetsDir: "assets",
+  },
+  optimizeDeps: {
+    exclude: ["sonner"], 
   },
 });
